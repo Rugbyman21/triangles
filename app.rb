cb4ed66b182be8cec46e47111e60babd3ea21090
Kellen Kolbeck & Trevor Scherling <student@epicodus.com>
2015-08-10 16:04:11 -0700

    It will add code that will load the pages of the application correctly.

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/triangles')
+
+get('/') do
+  erb(:index)
+  end
+
+get('/triangles') do
+  a_side=params.fetch('a_side')
+  b_side=params.fetch('b_side')
+  c_side=params.fetch('c_side')
+  @triangle=Triangle.new(a_side, b_side, c_side)
+  erb(:triangles)
+end
